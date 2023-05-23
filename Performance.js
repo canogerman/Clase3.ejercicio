62edc043dd2d67a342ba62f473f6bd8b4ecaa51b
canogerman <germancano80@gmail.com>
2023-05-23 08:19:28 -0300

    add file Performance.js

---
+++ Performance.js
@@ -0,0 +1,10 @@
+// Ejercicio: Imagina que estás evaluando el desempeño de los jugadores de la selección Argentina en el Mundial 2022 y deseas organizar la información de la siguiente manera utilizando solo arrays:
+
+// Tienes un array llamado scores que contiene los puntajes obtenidos por cada jugador en los partidos del Mundial.
+// Filtra los puntajes que son mayores a 7 y almacenalos en un nuevo array llamado outstandingScores.
+// Utiliza el array outstandingScores para crear un nuevo array llamado outstandingPlayers que contenga solo los nombres de los jugadores que obtuvieron puntajes destacados. Para ello, tendrás otro array llamado players que contiene los nombres de los jugadores en el mismo orden que el array scores.
+// Elimina los puntajes destacados del array original, dejando solo los puntajes regulares.
+// Crea un nuevo array regularPlayers  que tenga los nombres únicamente de los jugadores con un desempeño regular.
+
+// Implementa una función llamada evalPerformance que tome como parámetros dos arrays: scores (con los puntajes obtenidos por los jugadores) y players (nombres de dichos jugadores). La función debe aplicar las operaciones mencionadas anteriormente. De ser necesario atomizar la función en pequeñas funciones puras.
+

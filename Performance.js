45bcb5d04b73b25d735c207a3e684b36d7e45b2b
canogerman <germancano80@gmail.com>
2023-05-23 23:01:04 -0300

    Simplified code with Professor Santiago's recommendations

--- Performance.js
+++ Performance.js
@@ -10,32 +10,25 @@ const scores = [4, 5, 6, 7, 8, 9, 10, 4, 5, 6, 7, 8, 9, 10, 4, 5, 6, 100, 8, 9, 
 function evalPerformance(scores, players) {
     const outstandingScores = scores.filter(score => score > 7);
 
-    // I resolve outstandingPlayers atomically.
-    const bestPlayersIndexes = scores.map((score, index) => score > 7 ? index : false);
-    const outstandingPlayersIndexes = bestPlayersIndexes.filter(index => index !== false);
-    const outstandingPlayers = outstandingPlayersIndexes.map(index => players[index]);
+    // I resolve outstandingPlayers NO atomically!!.
+    const outstandingPlayers = players.filter((_, index) => scores[index] > 7);
 
-    // I resolve regularPlayers atomically before modifying the "scores" array.
-    const noBestPlayersIndexes = scores.map((score, index) => score <= 7 ? index : false);
-    const regularPlayersIndexes = noBestPlayersIndexes.filter(index => index !== false);
-    const regularPlayers = regularPlayersIndexes.map(index => players[index]);
+    // I resolve regularPlayers NO atomically!!.
+    const regularPlayers = players.filter((_, index) => scores[index] <= 7);
 
+    // I continue with the regular scores (NO deleting elements).
+    const regularScores = scores.filter(score => score <= 7);
+
+    // Imprimo los resultados.
     console.log('\x1b[4m%s\x1b[0m', 'Outstanding players and scores:');
     for (let i = 0; i < outstandingPlayers.length; i++) {
     console.log(`${outstandingPlayers[i]} - ${outstandingScores[i]}`);
     };
 
-    // I continue with the regular scores (deleting elements).
-    for (let i = outstandingPlayersIndexes.length - 1; i >= 0; i--) {
-        const index = outstandingPlayersIndexes[i];
-        scores.splice(index, 1);
-    };
-
     console.log('\x1b[4m%s\x1b[0m', 'Regular players and scores:');
     for (let i = 0; i < regularPlayers.length; i++) {
-    console.log(`${regularPlayers[i]} - ${scores[i]}`);
+    console.log(`${regularPlayers[i]} - ${regularScores[i]}`);
     };
 }
 
 evalPerformance(scores, players);
-
